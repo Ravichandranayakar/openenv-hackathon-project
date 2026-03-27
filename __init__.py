e405affc3d01b35f93976f2d061f80d7f32d412d
Ravichandranayakar <cary76800@gmail.com>
2026-03-27 19:29:55 +0530

    added init file to root

---
+++ __init__.py
@@ -0,0 +1,23 @@
+"""
+Customer Support OpenEnv Environment
+Main package initialization for OpenEnv CLI.
+"""
+
+# Import core components from my_env package
+from my_env import (
+    SupportAction,
+    SupportObservation,
+    CustomerSupportEnvironment,
+    CustomerSupportEnv,
+    TICKETS,
+    RESOLUTION_POLICIES,
+)
+
+__all__ = [
+    "SupportAction",
+    "SupportObservation",
+    "CustomerSupportEnvironment",
+    "CustomerSupportEnv",
+    "TICKETS",
+    "RESOLUTION_POLICIES",
+]
